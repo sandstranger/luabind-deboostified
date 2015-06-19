8705556d0078a5d969fdba52842f43ed70bc9507
Luke Benstead <kazade@gmail.com>
2015-06-19 09:25:26 +0100

    Fix warnings from clang by changing the type_list pop_back method implementation

--- luabind/detail/meta.hpp
+++ luabind/detail/meta.hpp
@@ -183,16 +183,35 @@ namespace meta {
 	/*
 	pop_back
 	*/
-	template< typename TypeN, typename... Types >
-	struct pop_back< type_list< Types..., TypeN > >
+
+	template< typename TypeN, typename IndexSequence >
+	struct insert_head;
+
+	template< typename TypeN, typename ...Types>
+	struct insert_head< TypeN, type_list<Types...> >
 	{
-		using type = type_list< Types... >;
+		typedef type_list< TypeN, Types... > type;
 	};
 
+	// Empty case
 	template< >
 	struct pop_back< type_list< > >
 	{
 		using type = type_list< >;
+	};
+
+	// Single element case
+	template< typename TypeN> struct pop_back< type_list< TypeN > >
+	{
+		typedef type_list<> type; // TypeN will be removed
+	};
+
+	// Normal case, recursively pop_back while pushing all but one onto the head
+	template< typename TypeN, typename... Types >
+	struct pop_back< type_list< TypeN, Types... > >
+	{
+		typedef typename pop_back< type_list<Types...> >::type tail;
+		typedef typename insert_head< TypeN, tail>::type type;
 	};
 
 	/*
