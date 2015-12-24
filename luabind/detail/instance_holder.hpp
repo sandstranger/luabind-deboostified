05a6879e5172f2dc7ab3c26cd5f318ac99e91535
ltjax <marius.elvert@googlemail.com>
2015-12-24 17:37:08 +0100

    Added missing overrides in instance_holder.hpp

--- luabind/detail/instance_holder.hpp
+++ luabind/detail/instance_holder.hpp
@@ -45,7 +45,7 @@ namespace luabind {
 			{
 			}
 
-			std::pair<void*, int> get(cast_graph const& casts, class_id target) const
+			std::pair<void*, int> get(cast_graph const& casts, class_id target) const override
 			{
 				// if somebody wants the smart-ptr, he can get a reference to it
 				if (target == registered_class<P>::id) return std::pair<void*, int>(&this->p, 0);
@@ -96,7 +96,7 @@ namespace luabind {
 				return true;
 			}
 
-			std::pair<void*, int> get(cast_graph const& casts, class_id target) const
+			std::pair<void*, int> get(cast_graph const& casts, class_id target) const override
 			{
 				const auto this_id = registered_class<ValueType>::id;
 				void* const naked_ptr = const_cast<void*>((const void*)&val_);
@@ -134,7 +134,7 @@ namespace luabind {
 				return val_ ? true : false;
 			}
 
-			std::pair<void*, int> get(cast_graph const& casts, class_id target) const
+			std::pair<void*, int> get(cast_graph const& casts, class_id target) const override
 			{
 				const auto value_id = registered_class<ValueType>::id;
 				void* const naked_value_ptr = const_cast<void*>((const void*) &val_);
