bcca69ea2b4ac2ba13668b9c984fcbfa114edca7
Michael Steinberg <michsteinb@gmail.com>
2017-07-06 23:07:14 +0200

    Preliminary fix of issue #27 . This issue is still under investigation.

--- luabind/out_value_policy.hpp
+++ luabind/out_value_policy.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2003 Daniel Wallin and Arvid Norberg
+﻿// Copyright (c) 2003 Daniel Wallin and Arvid Norberg
 
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
@@ -197,7 +197,7 @@ namespace luabind {
 			template<class T>
 			T& to_cpp(lua_State*, by_reference<T>, int)
 			{
-				storage_.template construct<T>();
+				storage_.decltype(storage_)::template construct<T>();
 				return storage_.template get<T>();
 			}
 
